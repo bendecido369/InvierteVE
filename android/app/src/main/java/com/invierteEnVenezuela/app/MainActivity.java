ae4e7866d53526795bdb87b01e6047760fc7ca0b
bendecido369 <godfirstbendecido@gmail.com>
2026-05-19 19:31:21 -0400

    Arreglar botón atrás en Android

--- android/app/src/main/java/com/invierteEnVenezuela/app/MainActivity.java
+++ android/app/src/main/java/com/invierteEnVenezuela/app/MainActivity.java
@@ -2,4 +2,12 @@ package com.invierteEnVenezuela.app;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {    @Override
+    public void onBackPressed() {
+        if (bridge != null && bridge.getWebView() != null && bridge.getWebView().canGoBack()) {
+            bridge.getWebView().goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }}
+
