de4c6d39dfbfa8687bdb1fa0efa920a30c0f5f31
bendecido369 <godfirstbendecido@gmail.com>
2026-05-19 21:03:44 -0400

    Update MainActivity.java

--- android/app/src/main/java/com/invierteEnVenezuela/app/MainActivity.java
+++ android/app/src/main/java/com/invierteEnVenezuela/app/MainActivity.java
@@ -1,13 +1,18 @@
 package com.invierteEnVenezuela.app;
 
+import android.view.KeyEvent;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {    @Override
-    public void onBackPressed() {
-        if (bridge != null && bridge.getWebView() != null && bridge.getWebView().canGoBack()) {
-            bridge.getWebView().goBack();
-        } else {
-            super.onBackPressed();
-        }
-    }}
+public class MainActivity extends BridgeActivity {
 
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (bridge != null && bridge.getWebView() != null && bridge.getWebView().canGoBack()) {
+                bridge.getWebView().goBack();
+                return true;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
